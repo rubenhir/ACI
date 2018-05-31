f24a140b3c11fd77cf4a06a685e12ec781448a57
ohynderi <ohynderi@gmail.com>
2018-05-31 17:46:57 +0200

    interim update

--- access_policies/leaf_prof_and_intf_pgr.py
+++ access_policies/leaf_prof_and_intf_pgr.py
@@ -33,6 +33,69 @@ def connect_aci(url, username, password):
         logger1.exception('Failed to connect to the controller')
 
 
+def create_intf_prof(md, config):
+
+    logger1.warning('Creating interface profile {0} with interface {1}, interface description {2} and policy group {3}'
+                    .format(config['interface_profile'], config['physical_interface'], config['interface_description'], config['interface_pgr']))
+
+    port_name = config['physical_interface'].split('/')[0] + '-' + config['physical_interface'].split('/')[1]
+    port_nbr = config['physical_interface'].split('/')[1]
+
+    '''
+    topDn = cobra.mit.naming.Dn.fromString('uni/infra/accportprof-' + config['interface_profile'])
+    topParentDn = topDn.getParent()
+    topMo = md.lookupByDn(topParentDn)
+    '''
+
+    polUni = cobra.model.pol.Uni('')
+    infraInfra = cobra.model.infra.Infra(polUni)
+
+    infraAccPortP = cobra.model.infra.AccPortP(infraInfra, name=config['interface_profile'])
+    infraHPortS = cobra.model.infra.HPortS(infraAccPortP, type='range', name=port_name, descr=config['interface_description'])
+    infraRsAccBaseGrp = cobra.model.infra.RsAccBaseGrp(infraHPortS, fexId='101', tDn=config['interface_pgr_dn'])
+    infraPortBlk = cobra.model.infra.PortBlk(infraHPortS, name='block2', descr='', fromPort=port_nbr, fromCard='1', toPort=port_nbr, toCard='1')
+
+
+    config['interface_profile_dn'] = infraAccPortP.dn
+
+    #print toXMLStr(infraInfra)
+    c = cobra.mit.request.ConfigRequest()
+    c.addMo(infraInfra)
+    #md.commit(c)
+
+
+def create_pgr(md, config):
+
+    polUni = cobra.model.pol.Uni('')
+    infraInfra = cobra.model.infra.Infra(polUni)
+    infraFuncP = cobra.model.infra.FuncP(infraInfra)
+
+    if re.match('PC_LACP', config['pgr_type']):
+        logger1.warning('Creating interface policy-group {0}'.format(config['interface_pgr']))
+        infraAccGrp = cobra.model.infra.AccBndlGrp(infraFuncP, name=config['interface_pgr'], lagT='link')
+
+    elif re.match('vPC_LACP', config['pgr_type']):
+        infraAccGrp = cobra.model.infra.AccBndlGrp(infraFuncP, name=config['interface_pgr'], lagT='node')
+
+    elif re.match('PC_ON', config['pgr_type']):
+        logger1.warning('Creating interface policy-group {0}'.format(config['interface_pgr']))
+        infraAccGrp = cobra.model.infra.AccBndlGrp(infraFuncP, name=config['interface_pgr'], lagT='link')
+
+    elif re.match('vPC_ON', config['pgr_type']):
+        infraAccGrp = cobra.model.infra.AccBndlGrp(infraFuncP, name=config['interface_pgr'], lagT='node')
+
+    elif re.match('Access', config['pgr_type']):
+        infraAccGrp = cobra.model.infra.AccPortGrp(infraFuncP, name=config['interface_pgr'])
+
+    config['interface_pgr_dn'] = infraAccGrp.dn
+    infraRsAttEntP = cobra.model.infra.RsAttEntP(infraAccGrp, tDn='uni/infra/attentp-' + config['aep'])
+
+    #print toXMLStr(infraFuncP)
+    c = cobra.mit.request.ConfigRequest()
+    c.addMo(infraFuncP)
+    #md.commit(c)
+
+
 def config_parser(fd):
     csv_file = csv.reader(fd, delimiter=',')
 
@@ -48,7 +111,7 @@ def config_parser(fd):
             result['physical_interface'] = line[4]
             result['physical_interface_speed'] = line[11]
             result['interface_description'] = line[5]
-            result['pgr_settings'] = line[6]
+            result['pgr_type'] = line[6]
             result['pgr_description'] = line[7]
             result['device_name'] = line[8]
 
@@ -58,23 +121,42 @@ def config_parser(fd):
             logger1.warning("Line {0} doesnt contain necessary information. Skipping... : {1}".format(line_id + 1, line))
 
 
-def config_sanity_check(config_parser):
-    for config in config_parser:
+def config_sanity_check(gen):
+    for config in gen:
         if config['switch_profile'] == '':
-            logger1.warning("Line {0} doesnt come with a valid switch profile. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug("Line {0} doesnt come with a valid switch profile. Skipping... : {1}".format(config['line_id'], config['line']))
 
         elif config['interface_profile'] == '':
-            logger1.warning("Line {0} doesnt come with a valid interface profile. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug("Line {0} doesnt come with a valid interface profile. Skipping... : {1}".format(config['line_id'], config['line']))
 
         elif config['interface_pgr'] == '':
-            logger1.warning("Line {0} doesnt come with a valid interface policy group. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug("Line {0} doesnt come with a valid interface policy group. Skipping... : {1}".format(config['line_id'], config['line']))
 
         elif config['aep'] == '':
-            logger1.warning("Line {0} doesnt come with a valid attach entity profile. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug("Line {0} doesnt come with a valid attach entity profile. Skipping... : {1}".format(config['line_id'], config['line']))
 
         elif not re.match('e[0-9]/[0-9]+', config['physical_interface']):
-            logger1.warning("Line {0} doesnt come with a valid interface. Skipping... : {1}".format(config['line_id'], config['line']))
-            logger1.warning("Expected interface syntax: e<x>/<y>")
+            logger1.debug("Line {0} doesnt come with a valid interface. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug("Expected interface syntax: e<x>/<y>")
+
+        elif not re.search('(10G|1G|100M)', config['physical_interface_speed']):
+            logger1.debug("Line {0} doesnt come with a valid interface speed. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug('Expected interface speed: 10G, 1G or 100M')
+
+        elif not re.search('(vPC_LACP|vPC_ON|PC_LACP|PC_ON|Access)', config['pgr_type']):
+            logger1.debug("Line {0} doesnt come with a valid policy group type. Skipping... : {1}".format(config['line_id'], config['line']))
+            logger1.debug('Expected policy group type: vPC_LACP, vPC_ON, PC_LACP, PC_ON or Access')
+
+        else:
+            yield config
+
+
+def apply_config(md, gen):
+
+    for config in gen:
+        create_pgr(md, config)
+        create_intf_prof(md, config)
+
 
 
 def main():
@@ -89,14 +171,15 @@ def main():
     parser_result = parser.parse_args()
 
     if parser_result.config_file and parser_result.ip and parser_result.username and parser_result.password:
-        md = connect_aci(parser_result.ip, parser_result.username, parser_result.password)
+        # md = connect_aci(parser_result.ip, parser_result.username, parser_result.password)
+        md = ''
 
         for file in [f for f in listdir(getcwd()) if isfile(join(getcwd(), f))]:
             if re.match(parser_result.config_file, file):
                 logger1.warning('Opening file {0}'.format(file))
 
                 with open(file, 'r') as fd:
-                    config_sanity_check(config_parser(fd))
+                    apply_config(md, config_sanity_check(config_parser(fd)))
 
     else:
         logger1.critical('Invalid argument provided')
